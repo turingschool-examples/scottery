bbf22d6c9f463ef8982a594264cb71fb5153f12d
novohispano <novohispano@gmail.com>
2015-02-17 00:25:51 -0700

    Use remote true in toggle

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+
+function toggleComments(button, target) {
+  $(button).on("click", function(){
+    var $showCommentsButton = $(this)
+
+    if ($showCommentsButton.text() === "Show Comments") {
+      $showCommentsButton.text("Hide Comments")
+    } else {
+      $showCommentsButton.text("Show Comments")
+    }
+
+    $(target).toggleClass("hide")
+  })
+}
--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,1 +1,15 @@
+function toggleComments(button, target) {
+  $(button).on("click", function(e){
+    e.preventDefault()
+    
+    var $showCommentsButton = $(this)
 
+    if ($showCommentsButton.text() === "Show Comments") {
+      $showCommentsButton.text("Hide Comments")
+    } else {
+      $showCommentsButton.text("Show Comments")
+    }
+
+    $(target).toggleClass("hide")
+  })
+}
